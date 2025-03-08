0afbf6ad1ad95b611583cccc342932d63f0023ca
Jerome Thayananthajothy <jerome.thavarshan@brompton.co.uk>
2025-03-08 20:24:19 +0530

    Add reveal functionality for secret with toggle button

--- resources/js/pages/Secret.vue
+++ resources/js/pages/Secret.vue
@@ -3,12 +3,13 @@ import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '@/componen
 import { Button } from '@/components/ui/button';
 import { Toaster, useToast } from '@/components/ui/toast';
 import { Head } from '@inertiajs/vue3';
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-import { AlertCircle, Copy } from 'lucide-vue-next';
+import { AlertCircle, Copy, Eye } from 'lucide-vue-next';
 import copy from 'copy-to-clipboard';
-
 import SecretContentInput from '@/components/SecretContentInput.vue';
+
+const revealed = ref(false);
 
 // Initialize form with a default expiration of 5 minutes
 const props = defineProps<{
@@ -38,20 +39,23 @@ function handleCopy() {
     <div class="flex min-h-screen flex-col items-center lg:justify-center p-4 md:p-8 w-full">
         <Card class="shadow-xl max-w-2xl">
             <CardHeader>
-                <CardTitle>Here is your secret</CardTitle>
+                <CardTitle>Secure secret!</CardTitle>
             </CardHeader>
             <CardContent class="space-y-4">
-                <Alert variant="destructive">
+                <Alert v-if="revealed" variant="destructive">
                     <AlertCircle class="w-4 h-4" />
                     <AlertTitle>This secret has been obliterated.</AlertTitle>
                     <AlertDescription>
                         Please be sure to store it in a safe place before closing this page.
                     </AlertDescription>
                 </Alert>
-                <SecretContentInput v-model="content" />
+                <SecretContentInput v-show="revealed" v-model="content" />
             </CardContent>
             <CardFooter>
-                <Button type="button" @click="handleCopy" class="w-full" size="lg">
+                <Button v-if="!revealed" type="button" @click="revealed = !revealed" class="w-full" size="lg">
+                    <Eye class="size-4 mr-1" /> Reveal secret
+                </Button>
+                <Button v-else type="button" @click="handleCopy" class="w-full" size="lg">
                     <Copy class="size-4 mr-1" /> Copy secret
                 </Button>
             </CardFooter>
