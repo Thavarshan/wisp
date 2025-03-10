80f37410e492d3de01b8e2356b535a7cae1a66ce
Jerome Thayananthajothy <jerome.thavarshan@brompton.co.uk>
2025-03-10 10:21:30 +0530

    Refactor PasswordProtection component layout for improved responsiveness and usability

--- resources/js/components/PasswordProtection.vue
+++ resources/js/components/PasswordProtection.vue
@@ -58,42 +58,35 @@ watch(isEnabled, async (value) => {
 </script>
 
 <template>
-    <div class="pt-2 flex items-start justify-between h-16">
-        <div>
-            <!-- Password Protect Toggle -->
-            <div class="flex items-center">
-                <Label class="flex items-start gap-3 cursor-pointer">
-                    <Checkbox v-model:checked="isEnabled" />
-                    <div class="flex flex-col">
-                        <span>Password protect</span>
-                        <p class="mt-1 text-xs text-muted-foreground w-56">
-                            For extra security, you have the option to set a password.
-                        </p>
-                    </div>
-                </Label>
-            </div>
-
-            <div>
-
-            </div>
+    <div class="pt-2 flex flex-col md:flex-row items-start justify-between md:h-16">
+        <!-- Password Protect Toggle -->
+        <div class="flex items-center">
+            <Label class="flex items-start gap-3 cursor-pointer">
+                <Checkbox v-model:checked="isEnabled" />
+                <div class="flex flex-col">
+                    <span>Password protect</span>
+                    <p class="mt-1 text-xs text-muted-foreground w-56">
+                        For extra security, you have the option to set a password.
+                    </p>
+                </div>
+            </Label>
         </div>
 
-        <div v-if="isEnabled">
+        <div v-if="isEnabled" class="mt-3 md:mt-0 w-full md:w-auto">
             <!-- Password Input and Copy Button (Shown only if enabled) -->
             <div class="relative flex items-center">
                 <Input
                     v-model="passwordValue"
                     type="text"
                     placeholder="Enter password"
-                    class="w-64 pr-10"
+                    class="w-full md:w-64 pr-10"
                 />
                 <Button
                     type="button"
                     variant="ghost"
                     size="icon"
                     @click="handleCopy"
-                    class="absolute right-0 mr-1"
-                >
+                    class="absolute right-0 mr-1">
                     <Copy class="size-4" />
                 </Button>
             </div>
